ffc8e1b83b5f816bdf8166ebf88da4d2dbbd29a7
Krincel <keltonwambu@gmail.com>
2025-03-29 13:56:04 +0000

    Add files via upload

---
+++ release/calculator.d
@@ -0,0 +1,1 @@
+C:\Users\Kelton\OneDrive\Desktop\Coding\ Stuff\My\ Codes\Rust\calculator\target\release\calculator.exe: C:\Users\Kelton\OneDrive\Desktop\Coding\ Stuff\My\ Codes\Rust\calculator\src\main.rs
