b61560e39fe1c4bd54eee0fc00eb7dd1cdb4cd34
Krincel <keltonwambu@gmail.com>
2025-04-19 20:16:37 +0100

    Delete release directory

--- release/calculator.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Kelton\OneDrive\Desktop\Coding\ Stuff\My\ Codes\Rust\calculator\target\release\calculator.exe: C:\Users\Kelton\OneDrive\Desktop\Coding\ Stuff\My\ Codes\Rust\calculator\src\main.rs
--- release/deps/calculator.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Kelton\OneDrive\Desktop\Coding Stuff\My Codes\Rust\calculator\target\release\deps\calculator.exe: src\main.rs
-
-C:\Users\Kelton\OneDrive\Desktop\Coding Stuff\My Codes\Rust\calculator\target\release\deps\calculator.d: src\main.rs
-
-src\main.rs:
